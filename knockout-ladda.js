761c74cd55448322878572508b7375de5d2a44a7
ericpsantos <ericsantos@outlook.com>
2015-09-05 12:28:30 -0700

    Create knockout-ladda.js
    http://jsfiddle.net/mbest/NwhpF/

---
+++ knockout-ladda.js
@@ -0,0 +1,17 @@
+
+ko.bindingHandlers.ladda = {
+    init: function (element, valueAccessor) {
+        var l = Ladda.create(element);
+
+        ko.computed({
+            read: function () {
+                var state = ko.unwrap(valueAccessor());
+                if (state)
+                    l.start();
+                else
+                    l.stop();
+            },
+            disposeWhenNodeIsRemoved: element
+        });
+    }
+};
